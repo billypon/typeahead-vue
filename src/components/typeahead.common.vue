5d60c8c5c3d2ebe1496052a01278915a90deab32
billypon <billypon@gmail.com>
2017-01-11 11:57:20 +0800

    code optimizes

--- src/components/typeahead.common.vue
+++ src/components/typeahead.common.vue
@@ -1,25 +1,27 @@
 <template lang="pug">
-.v-typeahead.form-control.dropdown(:class="{open: active, focus: focus, disabled: disabled}" @click="toggleDropdown")
+.v-typeahead.form-control.dropdown(:class="{open: active, focus: focus, disabled: disabled}" @click="toggleDropdown" @mousedown.prevent="")
   .v-typeahead-indicator.caret
-  .v-typeahead-text(v-if="!multiple && value") {{ getOptionLabel(value) }}
+  .v-typeahead-holder(:class="{highlight: !focus && !isValueEmpty}" v-show="!focus || !model") {{ isValueEmpty ? placeholder : !multiple && value ? getOptionLabel(value) : '' }}
   .v-typeahead-tag(v-if="multiple" v-for="option in value")
-    span.v-typeahead-tag-remove(@click="toggleOption(option)") &times;
+    span.v-typeahead-tag-remove(@click="toggleOption(option)" no-toggle) &times;
     | {{ getOptionLabel(option) }}
-  input.v-typeahead-input(ref="input"
-    v-model="model"
-    ,:placeholder="isValueEmpty ? placeholder : ''"
+  .v-typeahead-input(contenteditable
+    @input="model = $event.target.textContent"
     @focus="focus = true"
-    @blur="active = false; focus = false"
-    @keyup.up="inputKeyUp"
-    @keyup.down="inputKeyDown"
-    @keyup.enter="inputKeyEnter"
-    @keyup.esc="inputKeyEsc"
-    @keyup.delete="inputKeyDelete")
+    @blur="focus = active = false; clearInput()"
+    @keydown.up.prevent="onInputUp"
+    @keydown.down.prevent="onInputDown"
+    @keydown.enter.prevent="onInputEnter"
+    @keyup.esc="onInputEsc"
+    @keyup.delete="onInputDelete"
+    @keyup="onInputKeyUp"
+    @paste.prevent="onInputPaste"
+    ref="input"
+  )
   ul.v-typeahead-dropdown.dropdown-menu(:style="{'max-height': limit > 0 ? limit * 24 + 'px' : 'none'}" ref="list")
-    li(v-for="(option, index) in filteredOptions", :class="{active: isOptionSelected(option), hover: index === current}" @mousedown.prevent="toggleOption(option)" @mouseover="current = index") {{ getOptionLabel(option) }}
+    li(v-for="(option, index) in filteredOptions", :class="{active: isOptionSelected(option), hover: index === current}" @click="toggleOption(option)" @mouseover="current = index" no-toggle) {{ getOptionLabel(option) }}
     li.no-option(v-if="!filteredOptions.length")
       slot(name="no-options") no matching options
-  iframe.v-typeahead-tester(ref="tester")
 </template>
 
 <script>
--- src/typeahead.js
+++ src/typeahead.js
@@ -1,31 +1,22 @@
-function getPropertyValueSum (dom, ...props) {
-  let computedStyle = window.getComputedStyle(dom)
-  let sum = 0
-  for (let prop of props) {
-    sum += parseInt(computedStyle.getPropertyValue(prop))
-  }
-  return sum
-}
-
 export default {
   props: {
-    /*
-    * Contains the currently selected value. Very similar to a
-    * `value` attribute on an <input>. In most cases, you'll want
-    * to set this as a two-way binding, using :value.sync. However,
-    * this will not work with Vuex, in which case you'll need to use
-    * the change callback property.
-    */
+    //
+    // Contains the currently selected value. Very similar to a
+    // `value` attribute on an <input>. In most cases, you'll want
+    // to set this as a two-way binding, using :value.sync. However,
+    // this will not work with Vuex, in which case you'll need to use
+    // the change callback property.
+    //
     value: {
       default: null
     },
 
-    /*
-    * An array of strings or objects to be used as dropdown choices.
-    * If you are using an array of objects, vue-typeahead will look for
-    * a `label` key (ex. [{label: 'This is Foo', value: 'foo'}]). A
-    * custom label key can be set with the `label` prop.
-    */
+    //
+    // An array of strings or objects to be used as dropdown choices.
+    // If you are using an array of objects, vue-typeahead will look for
+    // a `label` key (ex. [{label: 'This is Foo', value: 'foo'}]). A
+    // custom label key can be set with the `label` prop.
+    //
     options: {
       type: Array,
       default () {
@@ -33,54 +24,59 @@ export default {
       }
     },
 
-    /*
-    * Equivalent to the `multiple` attribute on a `<select>` input.
-    */
+    //
+    // Equivalent to the `multiple` attribute on a `<select>` input.
+    //
     multiple: Boolean,
 
-    /*
-    * Equivalent to the `disabled` attribute on a `<select>` input.
-    */
+    //
+    // Equivalent to the `disabled` attribute on a `<select>` input.
+    //
     disabled: Boolean,
 
-    /*
-    * Equivalent to the `placeholder` attribute on an `<input>`.
-    */
+    //
+    // Equivalent to the `placeholder` attribute on an `<input>`.
+    //
     placeholder: {
       type: String,
       default: ''
     },
 
-    /*
-    * Tells vue-typeahead what key to use when generating option
-    * labels when each `option` is an object.
-    */
+    //
+    // If false, when press Enter, will select the first item of list automaticly.
+    //
+    disableEnter: Boolean,
+
+    //
+    // Tells vue-typeahead what key to use when generating option
+    // labels when each `option` is an object.
+    //
     label: {
       type: String,
       default: 'label'
     },
 
-    /*
-    * Tells vue-typeahead how many dropdown items to show.
-    */
+    //
+    // Tells vue-typeahead how many dropdown items to show.
+    //
     limit: {
       type: Number,
       default: 10
     },
 
-    /*
-    * An optional callback function that is called when filtering options.
-    * @param option
-    * @return {Boolean}
-    */
+    //
+    // An optional callback function that is called when filtering options.
+    // @param option
+    // @return {Boolean}
+    //
     filter: Function,
 
-    /*
-    * Callback to generate the label text. If {option}
-    * is an object, returns option[this.label] by default.
-    * @param option
-    * @return {String}
-    */
+    //
+    // Callback to generate the label text. If {option}
+    // is an object, returns option[this.label] by default.
+    // @param option
+    // @return {String}
+    //
     getOptionLabel: {
       type: Function,
       default (option) {
@@ -94,110 +90,121 @@ export default {
       active: false,
       focus: false,
       model: '',
-      current: -1,
-      oldModel: ''
+      current: -1
     }
   },
 
   methods: {
-    /**
-    * Toggle the visibility of the dropdown menu.
-    */
+    //
+    // Toggle the visibility of the dropdown menu.
+    //
     toggleDropdown (event) {
-      let target = event.target
-      let tag = target.tagName.toLowerCase()
-      if (this.disabled || tag === 'li') {
+      if (this.disabled || event.target.getAttribute('no-toggle')) {
         return
       }
+      this.active = !this.active
       this.current = -1
       this.$refs.list.scrollTop = 0
-      this.active = !this.active
-      this.$refs.input.focus()
+      this.active ? this.$refs.input.focus() : 0
     },
 
-    /**
-    * Toggle a given option.
-    */
-    toggleOption (option, focus) {
-      this.model = ''
+    //
+    // Toggle a given option.
+    //
+    toggleOption (option) {
       let value = this.value
       if (!this.multiple) {
         value = option
-        !focus ? this.$refs.input.blur() : 0
+        this.$refs.input.blur()
       } else {
         let index = value.indexOf(option)
         index === -1 ? value.push(option) : value.splice(index, 1)
+        this.clearInput()
       }
       this.applyValue(value)
     },
 
-    /*
-    * Check if the given option is currently selected.
-    *
-    * @param  option
-    * @return {Boolean}
-    */
+    //
+    // Check if the given option is currently selected.
+    //
+    // @param  option
+    // @return {Boolean}
+    //
     isOptionSelected (option) {
       return !this.multiple ? this.value === option : this.value.indexOf(option) !== -1
     },
 
-    inputKeyUp (event) {
-      if (!this.active) return
+    clearInput () {
+      this.model = this.$refs.input.textContent = ''
+    },
 
+    onInputUp (event) {
       this.current = (this.current > 0 ? this.current : this.filteredOptions.length) - 1
-      this.setListFocus()
-
-      !this.active ? this.toggleDropdown(event) : 0
+      this.hoverListItem()
     },
 
-    inputKeyDown (event) {
-      if (!this.active) return
-
+    onInputDown (event) {
       this.current = (this.current + 1 < this.filteredOptions.length ? this.current : -1) + 1
-      this.setListFocus()
-
-      !this.active ? this.toggleDropdown(event) : 0
+      this.hoverListItem()
     },
 
-    inputKeyEnter (event) {
-      if (!this.active) return
+    onInputEnter (event) {
+      if (!this.active || this.disableEnter) return
 
       let index = this.current
       if (index === -1) {
         if (!this.filteredOptions.length) return
         index = 0
       }
-      this.toggleOption(this.filteredOptions[index], true)
-      !this.multiple ? this.toggleDropdown(event) : 0
+      this.toggleOption(this.filteredOptions[index])
     },
 
-    inputKeyEsc () {
-      this.active ? this.active = false : this.model = ''
+    onInputEsc () {
+      this.active ? this.active = false : this.clearInput()
     },
 
-    inputKeyDelete (event) {
-      if (event.keyCode !== 8) return
-
-      let active = true
-
-      if (!this.model) {
-        active = false
-        if (!this.oldModel) {
-          let value = this.value
-          !this.multiple ? value = null : value.pop()
-          this.applyValue(value)
-        }
+    onInputDelete (event) {
+      if (!this.model && !this.oldModel && event.keyCode === 8) {
+        let value = this.value
+        !this.multiple ? value = null : value.pop()
+        this.applyValue(value)
       }
-
       this.oldModel = this.model
-      active && !this.active ? this.toggleDropdown(event) : 0
     },
 
-    setInputWidth () {
-      this.$refs.input.style.width = this.$el.clientWidth - getPropertyValueSum(this.$el, 'padding-right') - this.$refs.input.offsetLeft - 1 + 'px' // -1用于修正text小数宽度
+    onInputKeyUp (event) {
+      switch (event.keyCode) {
+        case 8: // backspace
+        case 46: // delete
+          if (this.model) break
+        case 13: // enter
+        case 27: // esc
+          return
+      }
+      !this.active ? this.toggleDropdown(event) : 0
     },
 
-    setListFocus () {
+    onInputPaste (event) {
+      let data = (event.clipboardData || clipboardData).getData('text')
+      if (!getSelection) {
+        document.execCommand("insertText", false, data)
+      } else {
+        let range = getSelection().getRangeAt(0)
+        let temp = document.createElement("span")
+        temp.innerHTML = "&#FEFF;"
+        range.deleteContents()
+        range.insertNode(temp)
+        let textRange = document.body.createTextRange()
+        textRange.moveToElementText(temp)
+        temp.parentNode.removeChild(temp)
+
+        textRange.text = data
+        textRange.collapse(false)
+        textRange.select()
+      }
+    },
+
+    hoverListItem () {
       if (!(this.limit > 0 && this.filteredOptions.length > this.limit)) return
 
       let unit = 24
@@ -216,52 +223,27 @@ export default {
   },
 
   computed: {
-    /*
-    * Check if there aren't any options selected.
-    *
-    * @return {Boolean}
-    */
+    //
+    // Check if there aren't any options selected.
+    //
+    // @return {Boolean}
+    //
     isValueEmpty () {
       return !this.multiple ? !this.value : !this.value || !this.value.length
     },
 
-    /*
-    * The currently displayed options, filtered
-    * by the search elements value.
-    *
-    * @return {array}
-    */
+    //
+    // The currently displayed options, filtered
+    // by the search elements value.
+    //
+    // @return {array}
+    //
     filteredOptions () {
       let model = this.model
       let filter = this.filter ? this.filter : option => option[this.label].indexOf(model) !== -1
       return this.options.filter(function (option) {
         return filter(option, model)
       })
-    }
-  },
-
-  mounted () {
-    let timeout
-    let width
-    this.$refs.tester.contentWindow.addEventListener('resize', () => {
-      // event will be fired immediatly
-      if (width === this.$refs.tester.clientWidth) return
-      width = this.$refs.tester.clientWidth
-      clearTimeout(timeout)
-      this.$refs.input.style.width = ''
-      timeout = setTimeout(() => {
-        this.setInputWidth()
-      }, 100)
-    })
-  },
-
-  updated () {
-    this.setInputWidth()
-  },
-
-  watch: {
-    value () {
-      this.$refs.input.style.width = '' // reset to minimum width
     }
   }
 }
